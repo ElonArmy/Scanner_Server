3bebdbba7ddf744a2b86c505939bfcf151cb72fa
EZHZY3 <comin6@gmail.com>
2024-01-12 00:28:20 +0900

    aws host update

--- scanproj/settings.py
+++ scanproj/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-f-y)(6ij%xa&+=#%=1r#m3fx-%!6&2aljn5@q73ft+5&u7g3r9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
